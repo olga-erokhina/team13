2a744a5ce228e3b2e6fec15d88cff13556dd8431
Jing_Sun <brian811117@gmail.com>
2021-03-19 17:47:21 +0100

    Changes to be committed: 	new file:   src/mdp_analysis/IO.py 	new file:   src/mdp_analysis/numerical.py 	new file:   src/mdp_analysis/statistical.py 	new file:   src/notebook_team0.ipynb 	new file:   src/notebook_team0.py 	modified:   src/notebook_unit1.ipynb

---
+++ src/mdp_analysis/IO.py
@@ -0,0 +1,108 @@
+import pandas as pd
+import numpy as np
+import seaborn as sn
+
+
+######## Input functions ########
+
+def read_df(filedir, filename):
+    """
+    Reads pandas dataframes.
+
+    Args:
+        filedir (string): the directory in which the file can be found.
+
+        filename (string): the name of the file.
+
+    Returns:
+        pandas DataFrame: containing the data from the file.
+    """
+
+    name = '{}{}'.format(filedir, filename)
+    print('Reading from file {} - pandas'.format(name))
+    data = pd.read_csv(name, r'\s+')
+    return data
+
+
+def read_np(filedir, filename):
+    """
+    Reads numpy arrays.
+
+    Args:
+        filedir (string): the directory in which the file can be found
+
+        filename (string): the name of the file.
+
+    Returns:
+        numpy array: containing the data from the file.
+    """
+
+    name = '{}{}'.format(filedir, filename)
+    print('Reading from file {} - numpy'.format(name))
+    data = np.loadtxt(name, skiprows=1)
+    data = data.T
+    return data
+
+
+################################
+####### Output functions #######
+
+def save_plot(figure, plotdir, filename):
+    """
+    Saves a figure to file
+
+    Args:
+        figure (seaborn Figure): the plot to save.
+
+        plotdir (string): the directory in which the plot should be saved.
+
+        filename (string): the name of the file to save in (with extension).
+
+    Returns:
+        Nothing
+    """
+
+    fig = figure.get_figure()
+    print('Saving figure at {}{}'.format(plotdir,filename))
+    fig.savefig('{}{}'.format(plotdir,filename))
+    return
+
+
+def save_df_data(df, filedir, filename):
+    """
+    Saves a pandas DataFrame to .csv-file
+
+    Args:
+        df (pandas Dataframe): the data to save.
+
+        filedir (string): the directory in which the data should be saved.
+
+        filename (string): the name of the file to save in (with extension).
+
+    Returns:
+        Nothing
+    """
+
+    print('Saving data at {}{}'.format(filedir,filename))
+    df.to_csv('{}{}'.format(filedir, filename), index=False)
+    return
+
+
+def save_np_data(arr, filedir, filename):
+    """
+    Saves a numpy array to .csv-file
+
+    Args:
+        arr (numpy array): the data to save.
+
+        filedir (string): the directory in which the data should be saved.
+
+        filename (string): the name of the file to save in (with extension).
+
+    Returns:
+        Nothing
+    """
+
+    print('Saving data at {}{}'.format(filedir,filename))
+    np.savetxt(fname = '{}{}'.format(filedir,filename), X = arr)
+    return
---
+++ src/mdp_analysis/statistical.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+import seaborn as sn
+
+
+######## Input functions ########
+
+def check_if_significant(data, thresh=1e-5):
+    """
+    trim data based on threshold.
+
+    Args:
+        pandas DataFrame: data needs to be trimmed.
+
+        threshold (float): a constant. Default = 1e-5.
+
+    Returns:
+        pandas DataFrame: data after trimming.
+        pandas DataFrame: corrsponding index of data.
+    """
+
+    data_out = data.drop(data.var()[data.var()<thresh].index.values, axis=1)
+    indices = data.var()[data.var() > thresh].index.values
+    return data_out, indices
+
+def get_correlation_measure(df):
+    """
+    Get correlation of data.
+
+    Args:
+        pandas DataFrame: data needs for correlation.
+
+    Returns:
+        pandas DataFrame: correlation.
+    """
+    drop_values = set() # an unordered collection of items
+    cols = df.columns # get the column labels
+    print(cols)
+    for i in range(0, df.shape[1]):
+        for j in range(0, i+1): # get rid of all diagonal entries and the lower triangular
+            drop_values.add((cols[i], cols[j]))
+    corr2 = df.corr().unstack() # pivot the correlation matrix
+    corr2 = corr2.drop(labels=drop_vals).sort_values(ascending=False, key=lambda col: col.abs()) # sort by absolute values but keep sign
+    return corr2
+
+def euclidean_distance(list_ref, list_comp, vectors):
+    """
+    Get correlation of data.
+
+    Args:
+        List: index of column for reference.
+        List: index of column for comparison.
+        Numpy DataFrame: Data stored in Matrix.
+
+    Returns:
+        Numpy DataFrame: Euclidean_distance.
+    """
+    distances = np.zeros(len(list_ref))
+    for i in range(len(list_ref)):
+        distances[i] = np.linalg.norm(vectors[list_comp[i]] - vectors[list_ref[i]])
+    return disances
